25a67a3493b926d90c595409cbc3a3991cdff27f
András Körmendi <psyplaysgames@gmail.com>
2024-12-30 18:05:51 +0100

    add linter and formatter

--- test/configs/environment.config.js
+++ test/configs/environment.config.js
@@ -3,5 +3,5 @@ import 'dotenv/config'
 export const API = {
   url: 'https://api.trello.com',
   key: process.env.API_KEY || '<your-api-key>',
-  token: process.env.API_TOKEN || '<your-api-token>'
+  token: process.env.API_TOKEN || '<your-api-token>',
 }
--- test/specs/board.spec.js
+++ test/specs/board.spec.js
@@ -1,81 +1,92 @@
-import request from 'supertest';
-import { API } from '../configs/environment.config.js';
-import { expect } from 'chai';
+import request from "supertest";
+import { API } from "../configs/environment.config.js";
+import { expect } from "chai";
+import { describe, it, before } from "mocha";
 
-let boardName = 'whateverBoard';
+let boardName = "whateverBoard";
 let boardId;
 let response;
 
 describe(`POST - Create a board`, () => {
   before(async () => {
-    response = await request(API.url).post(`/1/boards/?name=${boardName}&key=${API.key}&token=${API.token}`).then(response => response);
+    response = await request(API.url)
+      .post(`/1/boards/?name=${boardName}&key=${API.key}&token=${API.token}`)
+      .then((response) => response);
     boardId = response.body.id;
   });
-  
-  it('returns with CODE 200', () => {
+
+  it("returns with CODE 200", () => {
     expect(response.status).to.equal(200);
   });
-  
-  it('returns with application/json content-type header', () => {
-    expect(response.headers['content-type']).to.match(/application\/json/);
+
+  it("returns with application/json content-type header", () => {
+    expect(response.headers["content-type"]).to.match(/application\/json/);
   });
 
-  it('returns the created board as json', () => {
-    expect(response.body).to.have.property('name', boardName);
-  });
-});
-
-describe('GET - Read a board', () => {
-  before(async () => {
-    response = await request(API.url).get(`/1/boards/${boardId}?key=${API.key}&token=${API.token}`).then(response => response);
-  });
-
-  it('returns with a CODE of 200', () => {
-    expect(response.status).to.be.equal(200);
-  });
-
-  it('returns with application/json content-type header', () => {
-    expect(response.headers['content-type']).to.match(/application\/json/);
-  });
-
-  it('returns the requested board as json', () => {
-    expect(response.body).to.have.property('id', boardId);
-  });  
-});
-
-describe('PUT - Update a board', () => {
-  before(async () => {
-    boardName = 'someBoard';
-    response = await request(API.url).put(`/1/boards/${boardId}?name=${boardName}&key=${API.key}&token=${API.token}`).then(response => response);
-  });
-
-  it('returns with a CODE of 200', () => {
-    expect(response.status).to.be.equal(200);
-  });
-
-  it('returns with application/json content-type header', () => {
-    expect(response.headers['content-type']).to.match(/application\/json/);
-  });
-
-  it('returns the updated board as json', () => {
-    expect(response.body).to.have.property('name', boardName);
+  it("returns the created board as json", () => {
+    expect(response.body).to.have.property("name", boardName);
   });
 });
 
-describe('DELETE - Delete a board', () => {
+describe("GET - Read a board", () => {
   before(async () => {
-    response = await request(API.url).delete(`/1/boards/${boardId}?key=${API.key}&token=${API.token}`).then(response => response);
+    response = await request(API.url)
+      .get(`/1/boards/${boardId}?key=${API.key}&token=${API.token}`)
+      .then((response) => response);
   });
 
-  it('returns with CODE 200', () => {
+  it("returns with a CODE of 200", () => {
     expect(response.status).to.be.equal(200);
   });
 
-  it('returns with application/json content-type header', () => {
-    expect(response.headers['content-type']).to.match(/application\/json/);
+  it("returns with application/json content-type header", () => {
+    expect(response.headers["content-type"]).to.match(/application\/json/);
   });
 
-  it('returns with _value null in json', () => {
-    expect(response.body).to.have.property('_value', null);
+  it("returns the requested board as json", () => {
+    expect(response.body).to.have.property("id", boardId);
+  });
+});
+
+describe("PUT - Update a board", () => {
+  before(async () => {
+    boardName = "someBoard";
+    response = await request(API.url)
+      .put(
+        `/1/boards/${boardId}?name=${boardName}&key=${API.key}&token=${API.token}`,
+      )
+      .then((response) => response);
+  });
+
+  it("returns with a CODE of 200", () => {
+    expect(response.status).to.be.equal(200);
+  });
+
+  it("returns with application/json content-type header", () => {
+    expect(response.headers["content-type"]).to.match(/application\/json/);
+  });
+
+  it("returns the updated board as json", () => {
+    expect(response.body).to.have.property("name", boardName);
+  });
+});
+
+describe("DELETE - Delete a board", () => {
+  before(async () => {
+    response = await request(API.url)
+      .delete(`/1/boards/${boardId}?key=${API.key}&token=${API.token}`)
+      .then((response) => response);
+  });
+
+  it("returns with CODE 200", () => {
+    expect(response.status).to.be.equal(200);
+  });
+
+  it("returns with application/json content-type header", () => {
+    expect(response.headers["content-type"]).to.match(/application\/json/);
+  });
+
+  it("returns with _value null in json", () => {
+    expect(response.body).to.have.property("_value", null);
   });
 });
